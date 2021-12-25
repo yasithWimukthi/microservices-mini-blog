8b72cf6b50a86cdbf401b1c968faf75bb69b9453
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-25 10:08:27 +0530

    implement client service

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        Microservice blog
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
-}
-
+};
 export default App;
