1c5fb993c67ec69bd43ad68f7d093ae49370eed2
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-25 10:18:12 +0530

    implement client service

--- comments/index.js
+++ comments/index.js
@@ -5,6 +5,17 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+
+  next();
+});
+
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
